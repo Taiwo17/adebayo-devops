e254d23489e47228d35dd7582cb90cff0fdb9592
Taiwo Shobo <62107703+Taiwo17@users.noreply.github.com>
2026-08-17 09:29:02 +0100

    feat: add system information script (#1)
    * feat: add system information script
    
    * refactor: simplify OS information command

---
+++ scripts/system-info.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "=========================="
+echo "Adebayo System Information"
+echo "=========================="
+
+echo "Current User:"
+whoami
+
+echo "Hostname:"
+hostname
+
+echo "Kernel:"
+uname -r
+
+echo "Operating System:"
+grep PRETTY_NAME /etc/os-release
+
+echo "IP Address:"
+hostname -I
+
+echo "System Uptime:"
+uptime -p
